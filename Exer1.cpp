ba685f30a907693f4ca5db7f08eb28b0e4b0d88f
SaintWalker122 <45932323+SaintWalker122@users.noreply.github.com>
2018-12-17 15:51:30 +0800

    Add files via upload

---
+++ Exer1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+void max(){
+	
+int x, y, compare;
+
+
+ 
+if( x > y){
+
+cout<< x;
+}
+ else {
+ 	
+ 	cout<< y; 
+ }
+ 	
+ 
+ 
+	
+}
+
+
+int main() {
+	
+int x, y,ch;
+bool damn = true;
+
+
+while(damn)	{
+	
+cout<<" 1 - Find the greatest number1"<< endl;
+cout<<" 2 -  Exit"<< endl;	
+cin>> ch;
+
+
+		
+switch(ch){
+	
+case 1:
+	max();
+	
+	cout<<" ENTER A DAMN NUMBER"<< endl;
+cin>> x; 
+
+cout<<" ENTER A DAMN NUMBER"<< endl;
+cin>> y;
+
+
+
+ 	
+}	
+	
+}
+	
+	
+	
+return 0;	
+}
+
+
+
+
+	
+
